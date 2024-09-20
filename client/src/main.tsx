c4b0acfbb7c29a8ad557ef436754d33a3b783b02
michael1233 <emmamichael201@gmail.com>
2024-09-20 14:06:35 +0100

    created : react app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ChakraProvider>
     <App />
-  </StrictMode>,
+    </ChakraProvider>
+  </StrictMode>
 )
+
