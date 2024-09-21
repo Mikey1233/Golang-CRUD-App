00006387f1acbf888ba133111fda60599c4cb272
michael1233 <emmamichael201@gmail.com>
2024-09-21 12:13:45 +0100

    run build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 import TodoForm from "./components/TodoForm"
 import TodoList from "./components/TodoList"
 
-export const BASE_URL =import.meta.env.MODE === 'development'? "http://localhost:5000/api":'/api' 
+export const BASE_URL =import.meta.env.MODE === 'development'? "http://localhost:5000/api":'https://golang-crud-app-production.up.railway.app/api' 
 function App() {
   return (
     <Stack h="100vh">
